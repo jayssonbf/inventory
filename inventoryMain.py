2c97ec57bb9938e0d38494303021bb55a8232cb4
GovindaJR <gjramrattan2135@eagle.fgcu.edu>
2022-11-26 22:46:07 -0500

    Added backend functionality

--- inventoryMain.py
+++ inventoryMain.py
@@ -1,55 +1,173 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox
 from PyQt5 import uic
-import source
 import sys
+import scanner
+import database
+
 
 class MainWindow(QDialog):
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi("main_screen.ui", self)
-        self.searchButton.clicked.connect(self.gotoProductScreen)
+        self.setWindowTitle("Inventory Management System")
+        self.searchButton.clicked.connect(self.search)
+        self.scanButton.clicked.connect(self.scanBarcode)
+        self.clearButton.clicked.connect(self.clearInput)
+        self.show()
 
-    def gotoProductScreen(self):
-        widget.setCurrentIndex(widget.currentIndex()+1)
+    def search(self):
+        batch_number = self.batchInput.text()
+        product_id = self.productInput.text()
+        prod_type = self.typeDropdown.currentText()
+
+        # If all of the inputs are empty
+        if not batch_number and not product_id and not prod_type:
+            self.showBlankPopup()
+
+        else:
+            # Get data from inputs and check database
+            data = ""
+            if batch_number:
+                data = database.get_product_from_batch(batch_number)
+            elif product_id:
+                data = database.get_product_from_id(product_id)
+            elif prod_type:
+                data = database.get_product_by_type(prod_type)
+
+            # If a match is found in database, open Product Screen
+            if data:
+                self.gotoProductScreen(data)
+
+            # If no match is found, ask the user to add the product
+            else:
+                self.showAddItemQuery()
+
+    def scanBarcode(self):
+        batch = scanner.scan_for_barcode()
+        self.batchInput.setText(str(batch))
+
+    def showBlankPopup(self):
+        box = QMessageBox()
+        box.setWindowTitle("Error!")
+        box.setIcon(QMessageBox.Critical)
+        box.setText("Please fill out one of the fields.")
+        box.exec_()
+
+    def gotoProductScreen(self, data):
+        self.product_screen = ProductScreen(data)
+        self.product_screen.show()
+        self.close()
+
+    def showAddItemQuery(self):
+        box = QMessageBox()
+        result = box.question(self, 'Not Found',
+                              "No product with the entered "
+                              "information has been found. Would "
+                              "you like to add that item?",
+                              box.Yes | box.No)
+        if result == box.Yes:
+            self.add_screen = AddScreen()
+            self.add_screen.show()
+            self.close()
+
+    def clearInput(self):
+        self.batchInput.clear()
+        self.productInput.clear()
+
 
 class ProductScreen(QDialog):
-    def __init__(self):
+    def __init__(self, products):
         super(ProductScreen, self).__init__()
         uic.loadUi('product_screen.ui', self)
-        self.backLink.clicked.connect(lambda: gotoMain(self))
-        self.addProduct.clicked.connect(lambda: gotoAddProduct(self))
+        self.setWindowTitle("Inventory Management System")
+        self.products = products
+        self.loadTable()
+        self.backLink.clicked.connect(self.gotoMain)
+        self.addProduct.clicked.connect(self.gotoAddProduct)
+        self.show()
 
-        def gotoMain(self):
-            widget.setCurrentIndex(widget.currentIndex() - 1)
+    def gotoMain(self):
+        self.main_window = MainWindow()
+        self.main_window.show()
+        self.close()
 
-        def gotoAddProduct(self):
-            widget.setCurrentIndex(widget.currentIndex() + 1)
+    def gotoAddProduct(self):
+        self.add_screen = AddScreen()
+        self.add_screen.show()
+        self.close()
+
+    def loadTable(self):
+        self.productTable.verticalHeader().setVisible(False)
+        self.productTable.setRowCount(20)
+        # If provided data, populate the table
+        if self.products:
+            table_row = 0
+            for product in self.products:
+                for table_column, product_attribute in enumerate(product):
+                    self.productTable.setItem(table_row, table_column,
+                                              QtWidgets.QTableWidgetItem(
+                                                  str(product[table_column])))
+                table_row += 1
+
 
 class AddScreen(QDialog):
     def __init__(self):
         super(AddScreen, self).__init__()
         uic.loadUi('add_screen.ui', self)
+        self.setWindowTitle("Inventory Management System")
         self.goBack.clicked.connect(self.gotoMain)
+        self.createButton.clicked.connect(self.createItem)
+        self.cancelButton.clicked.connect(self.gotoMain)
 
     def gotoMain(self):
-        widget.setCurrentIndex(widget.currentIndex() - 2)
+        self.main_window = MainWindow()
+        self.main_window.show()
+        self.close()
 
-#window
+    def createItem(self):
+        name = self.nameInput.text()
+        batch = self.batchInput.text()
+        status = self.statusInput.text()
+        type = self.type.currentText()
+        quantity = self.quantityInput.text()
+        date = self.dateInput.text()
+        description = self.descriptionInput.toPlainText()
+
+        # Ensure no input is blank
+        if not (name and batch and status and type and quantity and date and
+                description):
+            self.showWarningMessage()
+
+        else:
+            database.add_product(name, type, int(batch), description,
+                                 int(quantity), status, date)
+
+            self.showSuccessMessage()
+            self.gotoMain()
+
+    def showWarningMessage(self):
+        box = QMessageBox()
+        box.setIcon(QMessageBox.Critical)
+        box.setWindowTitle("Error!")
+        box.setText("Please make sure all fields are entered.")
+        box.exec_()
+
+    def showSuccessMessage(self):
+        box = QMessageBox()
+        box.setWindowTitle("Success!")
+        box.setText("Product successfully added to the inventory!")
+        box.exec_()
+
+
+# window
 app = QApplication(sys.argv)
-widget = QtWidgets.QStackedWidget()
-mainwindow = MainWindow()
-productscreen = ProductScreen()
-addscreen = AddScreen()
-
-widget.addWidget(mainwindow)
-widget.addWidget(productscreen)
-widget.addWidget(addscreen)
+widget = MainWindow()
 
 widget.setWindowTitle('Inventory Management System')
 widget.setFixedHeight(600)
 widget.setFixedWidth(840)
+# widget.setStyleSheet('background: #FFFFFF;')
 widget.show()
 
 sys.exit(app.exec_())
-
---
+++ scanner.py
@@ -0,0 +1,58 @@
+"""database.py: Handles the functionality relating to the scanner"""
+import cv2
+from pyzbar import pyzbar
+import imutils
+
+
+# Thrown exception when barcode is found
+class FoundBarcode(Exception):
+    pass
+
+
+def close_camera(camera):
+    camera.release()
+    cv2.destroyAllWindows()
+
+
+# Reading barcode algorithm
+def read_barcodes(frame):
+    barcodes = pyzbar.decode(frame)
+    for barcode in barcodes:
+        x, y, w, h = barcode.rect
+        barcode_info = barcode.data.decode('utf-8')
+        # Record the barcode into a file
+        with open("barcode_result.txt", mode='w') as file:
+            file.write(barcode_info)
+        raise FoundBarcode
+    return frame
+
+
+def scan_for_barcode():
+    # Camera feed connects to network camera on phone
+    camera = cv2.VideoCapture("https://10.0.0.242:8080/video")
+
+    # To use web cam (default web cam on device):
+    #camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+    ret, frame = camera.read()
+    try:
+        # Loop searching for Barcode
+        while ret:
+            ret, frame = camera.read()
+            frame = imutils.resize(frame, width=800, height=600)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, "Press ESC to exit", (250, 50), font, 1.0,
+                         (255, 255, 255), 1)
+            frame = read_barcodes(frame)
+            cv2.imshow('Barcode/QR code reader', frame)
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
+
+        # Turn off video feed if user presses "Esc"
+        close_camera(camera)
+
+    except FoundBarcode:
+        close_camera(camera)
+        with open('barcode_result.txt') as f:
+            lines = f.readlines()
+        return int(lines[0])
